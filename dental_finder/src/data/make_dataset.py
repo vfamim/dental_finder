117958509f7067d3fa1c800f81fbd2bd843721a8
vfamim <vfamim@gmail.com>
2023-11-20 17:20:36 -0300

    save ingest data

--- dental_finder/src/data/make_dataset.py
+++ dental_finder/src/data/make_dataset.py
@@ -1,30 +1,46 @@
 # -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+from sqlalchemy import create_engine
+from pathlib import Path
+import pandas as pd
+import zipfile
+import logging
+import re
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def extract_data():
+    """Runs data processing scripts to turn raw data from (../raw)"""
+    raw_data_path = Path().resolve().parents[2] / 'data' / 'raw/'
+    with zipfile.ZipFile(raw_data_path / 'Archive+2.zip') as zip_file:
+        # removing the macos resources
+        file_names = [
+            f for f in zip_file.namelist() if not re.match('__MACOSX/', f)
+        ]
+        data = {}
+
+        for file_name in file_names:
+            with zip_file.open(file_name) as csv_file:
+                data[file_name] = pd.read_csv(csv_file)
+
+    return data
+
+
+def ingest_data(user, password, host, port, db):
+    eng = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    eng.connect()
+    table = extract_data()
+    for table_name in list(table):
+        table[table_name].to_sql(name=table_name, con=eng, if_exists='append')
+        logger = logging.getLogger(__name__)
+        logger.info(f'making {table_name} set from raw data')
 
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
 
-    main()
+    ingest_data()
